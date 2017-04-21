8dc3281f9978de396c910bf0c9ccba39216db208
Xtry333 <xtry333@gmail.com>
2017-04-21 22:21:50 +0200

    Added a simple list

--- Calculator02/Program.cs
+++ Calculator02/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +7,36 @@ namespace Calculator02
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-             
+            CustomList aList = new CustomList();
+
+            Console.WriteLine("#: {1}: {0}", aList, aList.Length);
+
+            aList.dodajNaPoczatek(new Node(1));
+            aList.dodajNaPoczatek(new Node(2));
+            aList.dodajNaPoczatek(new Node(3));
+            aList.dodajNaPoczatek(new Node(4));
+            aList.AddFirst(8);
+            aList.AddFirst(14);
+            aList.AddFirst(55);
+            aList.AddFirst(32);
+            aList.AddFirst(15);
+
+            aList.Add(69);
+            aList.Add(70);
+            aList.Add(71);
+            aList.Add(72);
+
+            Console.WriteLine("li[8]: " + aList[8]);
+            aList[8] = 5;
+            Console.WriteLine("#: {1}: {0}", aList, aList.Length);
+            aList.RemoveAt(7);
+            Console.WriteLine("li[0]: " + aList[0]);
+            Console.WriteLine("#: {1}: {0}", aList, aList.Length);
+
+            //lista.usunGlowe();
+            Console.ReadKey();
         }
     }
 }
---
+++ Calculator02/customList.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator02
+{
+    public class Node
+    {
+        public int value;
+        public Node nextNode;
+
+        public Node(int v)
+        {
+            this.value = v;
+            this.nextNode = null;
+        }
+
+        public int Length
+        {
+            get { return (nextNode == null ? 1 : 1 + nextNode.Length); }
+        }
+
+        public override string ToString()
+        {
+            if (nextNode == null)
+                return value + "";
+            return value + ", " + nextNode.ToString();
+        }
+    }
+
+    public class CustomList
+    {
+        public Node head = null;
+        public Node tail = null;
+        //public Node lastNode = null;
+
+        public void dodajNaPoczatek(Node node)
+        {
+            node.nextNode = head;
+            tail = head;
+            head = node;
+        }
+
+        public void AddFirst(int v)
+        {
+            Node node = new Node(v);
+            node.nextNode = head;
+            tail = head;
+            head = node;
+        }
+
+        public void Add(int v)
+        {
+            Node node = new Node(v);
+            Node lastNode = head;
+            Node prevNode = null;
+            //node.nextNode = null;
+            //Console.WriteLine("Head: " + head);
+            while(lastNode != null)
+            {
+                prevNode = lastNode;
+                lastNode = lastNode.nextNode;
+            }
+            //Console.WriteLine("LastNode: " + prevNode);
+            prevNode.nextNode = node;
+            //Console.WriteLine("Head: " + head);
+            //tail = head;
+            //head = node;
+        }
+
+        public void usunGlowe()
+        {
+            if (head != null)
+            {
+                head = tail;
+                if (tail != null)
+                    tail = tail.nextNode;
+            }
+        }
+
+        public void deleteSecond()
+        {
+            if (head != null)
+            {
+                if (head.nextNode != null)
+                    head.nextNode = head.nextNode.nextNode;
+            }
+        }
+
+        public int Length
+        {
+            get { return (head == null ? 0 : head.Length); }
+        }
+
+        public override string ToString()
+        {
+            if (head == null) return "NULL";
+            return head.ToString();
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= this.Length) throw new Exception("Index out of bounds");
+                tail = head;
+                while (index > 0)
+                {
+                    index--;
+                    tail = tail.nextNode;
+                }
+                if (index == 0) return tail.value;
+                return 0;
+            }
+            set
+            {
+                if (index >= this.Length) throw new Exception("Index out of bounds");
+                tail = head;
+                while (index > 0)
+                {
+                    index--;
+                    tail = tail.nextNode;
+                }
+                if (index == 0) tail.value = value;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index == 0) usunGlowe();
+            if (index >= this.Length) throw new Exception("Index out of bounds");
+            index--;
+            tail = head;
+            while (index > 0)
+            {
+                index--;
+                tail = tail.nextNode;
+            }
+            tail.nextNode = tail.nextNode.nextNode;
+        }
+
+    }
+}
