91bc7ba1e9a7d129f7353ad774f7fea1e45e56ef
Xtry333 <xtry333@gmail.com>
2017-04-23 05:19:08 +0200

    Pretty code & comments

--- Calculator02/Program.cs
+++ Calculator02/Program.cs
@@ -16,71 +16,31 @@ namespace Calculator02
 
         static void Main()
         {
-            CustomList aList = new CustomList();
             string input = "";
             bool quit = false;
-            bool infoDebug = false;
+            bool infoDebug = true;
 
             Console.ForegroundColor = ConsoleColor.Red;
-
-            //printList(aList);
             
-            //for(int i = 1; i <= 20; i++)
-            //{
-            //    if (i <= 10)
-            //        aList.Add(i.ToString());
-            //    else
-            //        aList.Push(i.ToString());
-            //}
-
-            //printList(aList);
-            //aList[5] = "^";
-            //printList(aList);
-            //Console.WriteLine("Pop: " + aList.Pop());
-            //Console.WriteLine("Peek: " + aList.Peek());
-            //Console.WriteLine("Pop: " + aList.Pop());
-            //aList[6] = "%";
-            //printList(aList);
-            ////aList.Reverse();
-            //printList(aList);
-            //aList.RemoveAt(0);
-            //printList(aList);
-            //aList.RemoveAt(2);
-            //printList(aList);
-
-            //for (int i = 0; i < 20; i++)
-            //{
-            //    if (i < aList.Count)
-            //        aList[i] = i.ToString();
-            //    else
-            //        aList.Add(i.ToString());
-            //}
-
-            //printList(aList);
-            //aList.Reverse();
-            //printList(aList);
-            //Console.ReadKey();
-            //Console.Clear();
-
             //while (!quit)
             {
                 Console.Clear();
                 Console.WriteLine("----------------------------");
                 Console.Write("Input <empty to exit>: ");
                 //input = Console.ReadLine();
-                input = "3+4*2/(10-5)^2";
+
+                //input = "3+4*2/(10-5)^2";
                 //input = "5*4-3+2-1*3/8*7+3";
                 //input = "5*(2-1)*2";
+                input = "((10 + (20 * 30)) - (0 - 15))";
 
-                if (input.Trim(' ') == "") quit = true;
+                if (input.Trim() == "") quit = true;
                 Console.WriteLine(input);
 
-                // captures numbers. Anything like 11 or 22.34 is captured
-                input = Regex.Replace(input, @"(?<number>\d+(\.\d+)?)", " ${number} ");
-                // captures these symbols: + - * / ^ ( )
-                input = Regex.Replace(input, @"(?<ops>[+\-*/^()])", " ${ops} ");
-                // trims up consecutive spaces and replace it with just one space
-                input = Regex.Replace(input, @"\s+", " ").Trim();
+                
+                input = Regex.Replace(input, @"(?<number>\d+(\.\d+)?)", " ${number} "); // Numbers           
+                input = Regex.Replace(input, @"(?<ops>[+\-*/^()])", " ${ops} "); // Tokens: + - * / ^ ( )
+                input = Regex.Replace(input, @"\s+", " ").Trim(); // Trim spaces
                 Console.WriteLine(input);
                 
                 string[] regInput = input.Split(" ".ToCharArray());
@@ -88,7 +48,7 @@ namespace Calculator02
                 CustomList operatorStack = new CustomList();
                 CustomList outputQueue = new CustomList();
 
-                string opstoken = "";
+                string cToken = ""; // Current Token from operatorStack
                 foreach (string s in regInput)
                 {
                     if (infoDebug)
@@ -106,94 +66,70 @@ namespace Calculator02
                         {
                             if (operatorStack.Count > 0)
                             {
-                                opstoken = operatorStack.Peek();
-                                // while there is an operator, o2, at the top of the stack
-                                while (opstoken == "+" || opstoken == "-" || opstoken == "*" || opstoken == "/" || opstoken == "^")
-                                {
-                                    // pop o2 off the stack, onto the output queue;
-                                    outputQueue.Add(operatorStack.Pop());
-                                    if (operatorStack.Count > 0)
+                                cToken = operatorStack.Peek();
+                                while (cToken == "+" || cToken == "-" || cToken == "*" || cToken == "/" || cToken == "^") // While there is an operator at the top
+                                {                                    
+                                    outputQueue.Add(operatorStack.Pop()); // pop it off the stack, onto the output queue
+                                    if (operatorStack.Count > 0) // if there is more
                                     {
-                                        opstoken = operatorStack.Peek();
-                                    }
-                                    else
-                                    {
+                                        cToken = operatorStack.Peek(); // go back to start
+                                    } else {
                                         break;
                                     }
                                 }
                             }
-                            // push o1 onto the operator stack.
                             operatorStack.Push(s);
                             if (infoDebug)
-                                Console.WriteLine("\tPushed #1: " + s);
+                                Console.WriteLine("\tPushed #1 (" + s + ")");
                         }
                         if (s == "*" || s == "/")
                         {
                             if (operatorStack.Count > 0)
                             {
-                                opstoken = operatorStack.Peek();
-                                // while there is an operator, o2, at the top of the stack
-                                while (opstoken == "+" || opstoken == "-" || opstoken == "*" || opstoken == "/" || opstoken == "^")
+                                cToken = operatorStack.Peek();
+                                while (cToken == "*" || cToken == "/" || cToken == "^") // While there is an operator at the top, that has higher priority than plus and minus
                                 {
-                                    if (opstoken == "+" || opstoken == "-")
+                                    outputQueue.Add(operatorStack.Pop()); // pop it off the stack, onto the output queue
+                                    if (operatorStack.Count > 0) // if there is more
                                     {
+                                        cToken = operatorStack.Peek(); // go back to start
+                                    } else {
                                         break;
-                                    }
-                                    else
-                                    {
-                                        // Once we're in here, the following algorithm condition is satisfied.
-                                        // o1 is associative or left-associative and its precedence is less than (lower precedence) or equal to that of o2, or
-                                        // o1 is right-associative and its precedence is less than (lower precedence) that of o2,
-
-                                        // pop o2 off the stack, onto the output queue;
-                                        outputQueue.Add(operatorStack.Pop());
-                                        if (operatorStack.Count > 0)
-                                        {
-                                            opstoken = operatorStack.Peek();
-                                        }
-                                        else
-                                        {
-                                            break;
-                                        }
                                     }
                                 }
                             }
-                            // push o1 onto the operator stack.
                             operatorStack.Push(s);
                             if (infoDebug)
-                                Console.WriteLine("\tPushed #2: " + s);
+                                Console.WriteLine("\tPushed #2 (" + s + ")");
                         }
                         if (s == "^" || s == "(")
                         {
                             operatorStack.Push(s);
                             if (infoDebug)
-                                Console.WriteLine("\tPushed #3: " + s);
+                                Console.WriteLine("\tPushed #3 (" + s + ")");
                         }
                         if (s == ")")
                         {
+                            if (infoDebug)
+                                Console.WriteLine("\tEncountered right parenthesis!");
                             if (operatorStack.Count > 0)
                             {
-                                opstoken = operatorStack.Peek();
-                                // Until the token at the top of the stack is a left parenthesis
-                                while (opstoken != "(")
-                                {
-                                    // pop operators off the stack onto the output queue
-                                    outputQueue.Add(operatorStack.Pop());
-                                    //Console.WriteLine("##Stack: " + operatorStack);
-                                    //Console.WriteLine("##Queue: " + outputQueue);
+                                cToken = operatorStack.Peek();                                
+                                while (cToken != "(") // Until the token at the top of the stack is a left parenthesis
+                                {                                    
+                                    outputQueue.Add(operatorStack.Pop()); // pop operators off the stack onto the output queue
+                                    if (infoDebug)
+                                        Console.WriteLine("\tEnqueue operator: " + cToken);
                                     if (operatorStack.Count > 0)
                                     {
-                                        opstoken = operatorStack.Peek();
-                                    }
-                                    else
-                                    {
+                                        cToken = operatorStack.Peek();
+                                    } else {
                                         // If the stack runs out without finding a left parenthesis,
                                         // then there are mismatched parentheses.
                                         throw new Exception("Unbalanced parenthesis!");
                                     }
-
                                 }
-                                // Pop the left parenthesis from the stack, but not onto the output queue.
+                                // Pop the left parenthesis from the stack
                                 operatorStack.Pop();
                             }
                         }
@@ -201,16 +137,16 @@ namespace Calculator02
                     if (infoDebug)
                     {
                         Console.WriteLine("@Stack: " + operatorStack);
-                        Console.WriteLine("Queue: " + outputQueue);
+                        Console.WriteLine("Output queue: " + outputQueue);
                     }
                     //Console.WriteLine("# - - - - -");
                 }
 
                 while (operatorStack.Count != 0)
                 {
-                    opstoken = operatorStack.Pop();
+                    cToken = operatorStack.Pop();
                     // If the operator token on the top of the stack is a parenthesis
-                    if (opstoken == "(")
+                    if (cToken == "(")
                     {
                         // then there are mismatched parenthesis.
                         throw new Exception("Unbalanced parenthesis!");
@@ -218,14 +154,14 @@ namespace Calculator02
                     else
                     {
                         // Pop the operator onto the output queue.
-                        outputQueue.Add(opstoken);
+                        outputQueue.Add(cToken);
                     }
                     if (infoDebug)
-                        Console.WriteLine("#Adding remaining operators: " + opstoken);
+                        Console.WriteLine("#Adding remaining operators: " + cToken);
                 }
 
+                Console.WriteLine("Output RPN: ");
                 printList(outputQueue);
-                printList(operatorStack);
             }
             Console.ReadKey();
             Console.WriteLine("Exiting...");
